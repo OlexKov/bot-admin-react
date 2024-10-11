d18bfa9cc8b3f27299ba14e425c15ebca4e8cf8c
OlexKov <Kovalchuk.olex@gmail.com>
2024-10-11 12:10:24 +0300

    Jwt exp date check added

--- src/App.tsx
+++ src/App.tsx
@@ -15,18 +15,6 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Users />} />
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/categories/create" element={
-            <AdminProtectedRoute children={<CategoryCreation />} />} />
-          <Route path="/categories" element={
-            <AdminProtectedRoute children={<CategoryTable />} />} />
-          <Route path="/products" element={
-            <AdminProtectedRoute children={<ProductTable />} />} />
-          <Route path="/products/create" element={
-            <AdminProtectedRoute children={<ProductCreate />} />} />
-          <Route path="/favorites" element={<FavoritesPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          
-          <Route path="/registration" element={<Registration />} /> */}
           <Route path="*" element={
             <Error
               status="404"
--- src/helpers/common-methods.ts
+++ src/helpers/common-methods.ts
@@ -5,37 +5,40 @@ import { jwtDecode } from "jwt-decode";
 export const TryError = async <T>(funct: Function): Promise<AxiosResponse<T, any>> => await funct().catch((error: any) => error)
 
 export const getDataFromJwtToken = (token: string | undefined | null): User | undefined => {
-    if(token){
-        const data: any = jwtDecode(token);
-        return {
-            id: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'],
-            name: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'],
-            surname: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname'],
-            email: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'],
-            exp: data['exp'],
-            iss: data['iss'],
-            roles: data['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'],
-            phone: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/homephone'],
-            image: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/anonymous'],
-        }
+  if (token) {
+    const data: any = jwtDecode(token);
+    const user: User = {
+      id: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'],
+      name: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'],
+      surname: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname'],
+      email: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'],
+      exp: data['exp'],
+      iss: data['iss'],
+      roles: data['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'],
+      phone: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/homephone'],
+      image: data['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/anonymous'],
     }
-    return undefined;
-    
+    if (new Date(user.exp * 1000) >= new Date()) {
+      return user;
+    }
+  }
+  return undefined;
+
 }
 
 export const getQueryString = (filter: any): string => {
-    let result = '';
-    Object.keys(filter).forEach((key) => {
-      if (filter[key] !== undefined
-        && filter[key] !== null
-        && filter[key] !== ''
-        && filter[key]?.length !== 0) {
-        const value = typeof (filter[key]) === "object"
-          ? JSON.stringify(filter[key])
-          : filter[key];
-        const symbol = result === '' ? '?' : '&'
-        result += `${symbol + key}=${value}`
-      }
-    });
-    return result;
-  } 
+  let result = '';
+  Object.keys(filter).forEach((key) => {
+    if (filter[key] !== undefined
+      && filter[key] !== null
+      && filter[key] !== ''
+      && filter[key]?.length !== 0) {
+      const value = typeof (filter[key]) === "object"
+        ? JSON.stringify(filter[key])
+        : filter[key];
+      const symbol = result === '' ? '?' : '&'
+      result += `${symbol + key}=${value}`
+    }
+  });
+  return result;
+} 
